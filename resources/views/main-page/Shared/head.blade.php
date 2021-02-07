8662ef9e6a293909a1fc1f274871cfb999e60ca0
Raymond Youssef <rimonomega@gmail.com>
2021-02-07 12:21:41 +0200

    fixed some bugs

--- resources/views/main-page/Shared/head.blade.php
+++ resources/views/main-page/Shared/head.blade.php
@@ -10,31 +10,31 @@
 <title>Doctor - Home</title>
 
 <!-- Standard Favicon -->
-<link rel="icon" type="image/x-icon" href="assets/images//favicon.ico" />
+<link rel="icon" type="image/x-icon" href={{ asset("assets/images/favicon.ico") }} />
 <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i|Montserrat+Alternates:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i|Playfair+Display:400,400i,700,700i,900,900i|Poppins:300,400,500,600,700|Quattrocento:400,700|Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
 
-<link rel="stylesheet" type="text/css" href={{asset('assets/revolution/css/settings.css')}}>
+<link rel="stylesheet" type="text/css" href={{asset('assets/revolution/css/settings.css')}} />
 
-<link rel="stylesheet" type="text/css" href={{asset('assets/revolution/fonts/font-awesome/css/font-awesome.css')}}>
-<link rel="stylesheet" type="text/css" href={{asset('assets/revolution/fonts/font-awesome/css/font-awesome.min.css')}}>
+<link rel="stylesheet" type="text/css" href={{asset('assets/revolution/fonts/font-awesome/css/font-awesome.css')}} />
+<link rel="stylesheet" type="text/css" href={{asset('assets/revolution/fonts/font-awesome/css/font-awesome.min.css')}} />
 
 <!-- RS5.0 Layers and Navigation Styles -->
-<link rel="stylesheet" type="text/css" href={{asset('assets/revolution/css/layers.css')}}>
-<link rel="stylesheet" type="text/css" href={{asset('assets/revolution/css/navigation.css')}}>
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css" integrity="sha384-Bfad6CLCknfcloXFOyFnlgtENryhrpZCe29RTifKEixXQZ38WheV+i/6YWSzkz3V" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href={{asset('assets/revolution/css/layers.css')}} />
+<link rel="stylesheet" type="text/css" href={{asset('assets/revolution/css/navigation.css')}} />
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css" integrity="sha384-Bfad6CLCknfcloXFOyFnlgtENryhrpZCe29RTifKEixXQZ38WheV+i/6YWSzkz3V" crossorigin="anonymous" />
 
 
 <!-- Library - Bootstrap v3.3.5 -->
-<link href={{asset('assets/css/lib.css" rel="stylesheet')}}>
+<link rel="stylesheet" href={{asset('assets/css/lib.css')}} />
 
-<link rel="stylesheet" href={{asset('assets/js/slick/slick.css')}} >
-<link  rel="stylesheet" href={{asset('assets/js/slick/slick-theme.css')}} >
+<link rel="stylesheet" href={{asset('assets/js/slick/slick.css')}} />
+<link  rel="stylesheet" href={{asset('assets/js/slick/slick-theme.css')}} />
 
 <!-- Custom - Common CSS -->
-<link  rel="stylesheet" href={{asset('assets/css/plugins.css')}} >
-<link href={{asset('assets/css/elements.css" rel="stylesheet')}}>
-<link href={{asset('assets/css/rtl.css" rel="stylesheet')}}s>
-<link id="color" href={{asset('assets/css/color-schemes/default.css')}} rel="stylesheet"/>
+<link  rel="stylesheet" href={{asset('assets/css/plugins.css')}} />
+<link rel="stylesheet" href={{asset('assets/css/elements.css')}} />
+<link rel="stylesheet" href={{asset('assets/css/rtl.css')}} />
+<link id="color" rel="stylesheet" href={{asset('assets/css/color-schemes/default.css')}} />
 
 <!-- Custom - Theme CSS -->
 <link rel="stylesheet" type="text/css" href={{asset('assets/style.css')}}>
