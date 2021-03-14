25e0fcf4d52e86687f706a66857840299b697c2b
Rana Medhat <rana.medhat69@gmail.com>
2021-03-14 16:19:25 +0200

    fix

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -46,29 +46,40 @@
         <div class="row">
             <div class="col-md-4">
                 <img src="assets/images/logo.jpg" />
+                <h1 class="name">Dr.Saber Waheeb</h1>
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt suscipit itaque illo, vero laborum
                     ipsa voluptatem temporibus nostrum! Assumenda tempora libero, dolores nobis autem possimus
                     voluptatibus accusamus voluptatum voluptas! Corporis.</p>
             </div>
-            <div class="col-md-6 section-two">
+            <div class="col-md-5 section-two">
                 <h6 class="footer-title">information</h6>
                 <div class="row mb">
-                    <div class="col-md-6 icon-container"> <i class="fa fa-phone" style="margin-top: 5px;margin-left: -5px;"></i></div>
+                    <div class="col-md-6 icon-container"> <i class="fa fa-phone" style="margin-left: -5px;"></i></div>
                     <span class="col-md-6">01224745749</span>
                 </div>
                 <div class="row mb">
-                    <div class="col-md-6 icon-container"> <i class="fa fa-envelope" style="margin-top: 5px;margin-left: -6px;"></i></div>
+                    <div class="col-md-6 icon-container"> <i class="fa fa-envelope" style="margin-left: -7px;"></i>
+                    </div>
                     <span class="col-md-6">example@example.com</span>
                 </div>
                 <div class="row mb">
-                    <div class="col-md-6 icon-container"> <i class="fa fa-map-marker" style="margin-top: 5px;margin-left: -4px;"></i></div>
-                    <span class="col-md-6">example@example.com</span>
+                    <div class="col-md-6 icon-container"> <i class="fa fa-map-marker" style="margin-left: -4px;"></i>
+                    </div>
+                    <span class="col-md-6">El-Gaish Rd, Al Mesallah Sharq, Al Attarin, Alexandria Governorate</span>
+                </div>
+                <div class="row mb">
+                    <div class="col-md-6 icon-container"> <i class="fa fa-facebook" style="margin-left: -4px;"></i>
+                    </div>
+                    <a href="https://www.facebook.com/prof.dr.saberwaheeb/"> <span class="col-md-6"
+                            style="margin-top: 5px">https://www.facebook.com/prof.dr.saberwaheeb/ </span></a>
                 </div>
 
+            </div>
+            <div class="col-md-2 map-container">
+                <iframe
 
-                {{-- <ul class="social">
-                    <li><i class="fa fa-facebook"></i></li>
-                </ul> --}}
+                    src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d109200.89588805466!2d29.877898748616953!3d31.206098178828555!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2seg!4v1615730149959!5m2!1sen!2seg"
+                    allowfullscreen="" loading="lazy"></iframe>
             </div>
         </div>
     </div>
