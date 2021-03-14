a65ea7dc7d0605b3c66ace7e2bd6839eb7502e2b
Raymond Youssef <rimonomega@gmail.com>
2021-03-14 20:33:33 +0200

    fix

--- app/Http/Controllers/Auth/ConfirmablePasswordController.php
+++ app/Http/Controllers/Auth/ConfirmablePasswordController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
-use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
@@ -14,10 +13,10 @@ class ConfirmablePasswordController extends Controller
     /**
      * Show the confirm password view.
      *
-     * @param Request $request
-     * @return View
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\View\View
      */
-    public function show(Request $request): View
+    public function show(Request $request)
     {
         return view('auth.confirm-password');
     }
@@ -25,15 +24,14 @@ class ConfirmablePasswordController extends Controller
     /**
      * Confirm the user's password.
      *
-     * @param Request $request
+     * @param  \Illuminate\Http\Request  $request
      * @return mixed
-     * @throws ValidationException
      */
     public function store(Request $request)
     {
         if (! Auth::guard('web')->validate([
             'email' => $request->user()->email,
-            'password' => $request['password'],
+            'password' => $request->password,
         ])) {
             throw ValidationException::withMessages([
                 'password' => __('auth.password'),
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
 use App\Http\Controllers\ArticleController;
-use App\Http\Controllers\EventController;
 use App\Http\Controllers\MainController;
-use App\Http\Controllers\OperationController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -50,24 +48,10 @@ Route::resource('articles', ArticleController::class);
 */
 //==============================================================================
 
-// dashboard
-Route::get('/dashboard', function () {
-    return view('dashboard/dashboard');
-});
 
-
-Route::get('/login', function () {
-    return view('dashboard/login');
-});
-
-
-
-Route::get('/dashboard', function () {
+Route::get('/dashboar', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
 
-Route::fallback(function () {
-    return 'Not Found!';
-});
+require __DIR__.'/auth.php';
